3fde625b07ec4213822374ec1f52c3dddd84ca9d
AZAIPA <159898151+241854@users.noreply.github.com>
2024-03-04 16:53:28 +0100

    Update azemfack.py

--- azemfack.py
+++ azemfack.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import streamlit as st
-import pandas_profiling 
+#import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 from fontawesome import icons
 from pycaret.regression import setup as setup_reg
